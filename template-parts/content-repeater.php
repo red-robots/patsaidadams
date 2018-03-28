b7cafe16fc92c904f3e6239953b04fea44e1ca5a
Fritz <fre.healy@gmail.com>
2018-03-28 14:31:06 -0400

    styling repeater page

--- template-parts/content-repeater.php
+++ template-parts/content-repeater.php
@@ -11,39 +11,25 @@
 
 <div id="post-<?php the_ID(); ?>" <?php post_class("template-repeater"); ?>>
     <div class="wrapper cap">
-        <?php $args = array(
-            'post_type'=>'post',
-            'posts_per_page'=>10,
-            'orderby'=>'date',
-            'order'=>'DESC'
-        );
-        $query = new WP_Query($args);
-        if ( $query->have_posts() ) : ?>
+        <?php $list = get_field("list");
+        if ( $list ) : ?>
             <?php /* Start the Loop */
-            while ( $query->have_posts() ) : $query->the_post();?>
-                <article class="post">
-                    <header>
-                        <h1><?php the_title();?></h1>
-                    </header>
-                    <div class="wrapper clear-bottom">
-                        <div class="col-1">
-                            <?php $image = get_field("image");
-                            if($image):?>
-                                <img src="<?php echo $image['sizes']['large'];?>" alt="<?php echo $image['alt'];?>">
-                            <?php endif;?>
-                        </div><!--.col-1-->
-                        <div class="col-2">
-                            <div class="date">
-                                <?php echo get_the_date('M d, Y');?>
-                            </div><!--.date-->
-                            <div class="copy">
-                                <?php the_content();?>
-                            </div><!--.copy-->
-                        </div><!--.col-2-->
-                    </div><!--.wrapper-->
+            foreach ($list as $item) : ?>
+                <article class="post clear-bottom">
+                    <div class="col-1">
+                        <?php $image = $item['image'];
+                        if($image):?>
+                            <img src="<?php echo $image['sizes']['large'];?>" alt="<?php echo $image['alt'];?>">
+                        <?php endif;?>
+                    </div><!--.col-1-->
+                    <div class="col-2 copy">
+                        <?php $copy = $item['copy'];
+                        if($copy):?>
+                            <?php echo $copy;?>
+                        <?php endif;?>
+                    </div><!--.col-2-->
                 </article><!--.post-->
-            <?php endwhile;?>
-            <?php wp_reset_postdata();
-        endif; ?>
+            <?php endforeach;?>
+        <?php endif; ?>
     </div><!--.wrapper-->
 </div><!--.template-category-->
